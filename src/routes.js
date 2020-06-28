72526020d4a52255941de377591564d3f0e3a90a
Werikson Rodrigues <werikson64@gmail.com>
2020-06-27 23:34:50 -0300

    atualização

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/home/home.vue';
+import Cadastro from './components/cadastro/cadastro.vue';
+
+export const routes = [
+    { path: '', component: Home , titulo:'Home'},
+    { path: '/cadastro', component: Cadastro , titulo:'Cadastro'}
+];
